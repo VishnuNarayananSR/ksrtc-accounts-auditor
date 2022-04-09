b364d7a2b1d7265e602509d694c29a9badb6171e
Vishnu Narayanan S R <srvishnunarayanan@gmail.com>
2022-04-10 02:00:30 +0530

    theme colors changed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,19 @@ import UploadForm from "./Components/UploadForm/UploadForm";
 function App() {
   return (
     <>
-      <nav>KSRTC Collection Audit</nav>
+      <style>
+        {`:root {
+        --color-primary: #1a746b;
+        --color-secondary: #ffab40;
+        --color-tertiary: #99d5cf;
+        --color-background: #E5E5E5;
+        --color-text: #e8eaf6;
+        --color-text-dark: #442c2e;
+        }`}
+      </style>
+      <nav style={{ background: "var(--color-primary)" }}>
+        KSRTC Collections Auditor
+      </nav>
       <UploadForm></UploadForm>
     </>
   );
