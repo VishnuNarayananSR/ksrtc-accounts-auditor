35ee1ba70dd98e66220578ec5bd3c4b1f8c72dd5
Vishnu Narayanan S R <vishnutheallmighty@gmail.com>
2022-05-09 18:47:38 +0530

    added db connection at the start of the app

--- pages/api/collection/diesel-price.js
+++ pages/api/collection/diesel-price.js
@@ -2,7 +2,7 @@ import dbConnect from "../../../db";
 import Collection from "../../../db/models/collection";
 
 export default async function getDieselPrice(req, res) {
-  await dbConnect();
+  // await dbConnect();
   try {
     const dieselPriceArr = await Collection.find().sort({ date: -1 }).limit(1);
     const dieselPrice = dieselPriceArr.length ? dieselPriceArr[0].dieselPrice : 0;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import UploadForm from "../Components/UploadForm/UploadForm";
 import MainLayout from "../Components/Layout/MainLayout";
-
+import { useEffect } from "react";
 export default function Home() {
+  useEffect(() => {
+    fetch("/api/db/connect");
+  }, []);
   return (
     <MainLayout>
       <UploadForm></UploadForm>
